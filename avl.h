1b0ce2ffe963bc9339cca076e8e4d4916ce0d603
Cole Lyman <cole@colelyman.com>
2013-03-30 23:47:14 -0600

    Initial implementation with Node and blank functions

--- avl.h
+++ avl.h
@@ -1,11 +1,64 @@
 #ifndef AVL_H
 #define AVL_H
+#include <string>
+#include <sstream>
 
-template<type ItemType>
+using std::string;
+using std::stringstream;
+using std::endl;
+
+template<typename ItemType>
 class set {
 private:
+	struct Node {
+		ItemType item;
+		Node* left;
+		Node* right;
+		int height;
 
+		Node() {
+			left = NULL;
+			right = NULL;
+			height = 0;
+		}
+
+		Node(Node* n) {
+			item = n->item;
+			left = n->left;
+			right = n->right;
+			height = n->height;
+		}
+
+		Node(ItemType item) {
+			this->item = item;
+			left = NULL;
+			right = NULL;
+			height = 0;
+		}
+	};
+	Node* root;
+	int size;
 public:
+	set() {
+		root = new Node();
+		size = 0;
+	}
+	~set() {
 
+	}
+	void add(const ItemType& item) {
+
+	}
+	void remove(const ItemType& item) {
+
+	}
+	bool find(const ItemType& item) {
+
+		return false;
+	}
+	string print() {
+
+		return "";
+	}
 };
 #endif
